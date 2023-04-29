1fbb913cd3281eb33080335bcdd7756ec8bab599
Sanika <sanikajoshi8903@gmail.com>
2023-04-29 23:28:15 +0530

    Type casting in java

---
+++ type_casting.java
@@ -0,0 +1,11 @@
+package miscellaneous;
+import java.util.*;
+
+public class type_casting {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    float a = sc.nextFloat();
+    int b = (int)a;
+    System.out.println(b);
+   } 
+}
