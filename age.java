d4c41016af977de69cac24eec5a67455dbd5cafa
Sanika <sanikajoshi8903@gmail.com>
2023-05-02 22:36:33 +0530

    initial commit

---
+++ age.java
@@ -0,0 +1,18 @@
+package miscellaneous;
+import java.util.*;
+
+public class age {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age>=18)
+        {
+            System.out.println("Eligible for voting");
+        }
+        else{
+            System.out.println("not elgible to vote");
+        }
+    }
+    
+}
